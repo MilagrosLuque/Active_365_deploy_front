e67ab1b016e75a1f5dc07bfb5a68b66eb9606e9c
MilagrosLuque <luquemariamilagros@gmail.com>
2025-01-13 13:06:07 -0300

    fix[category]

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -1,21 +1,8 @@
 import { getProductsByCategoryOrName } from '@/app/api/getProducts';
 import Card from '@/components/productsCard/Card';
-import { Metadata } from 'next';
 import React from 'react';
 
-// Definimos el tipo para los parámetros dinámicos
-type PageProps = {
-  params: {
-    category: string;
-  };
-};
-
-// Definir el metadata opcional
-export const metadata: Metadata = {
-  title: 'Products Page',
-};
-
-const Products: React.FC<PageProps> = async ({ params }) => {
+const Products: React.FC<{ params: { category: string } }> = async ({ params }) => {
   const { category } = params;
   const products = await getProductsByCategoryOrName(category);
 
@@ -23,11 +10,11 @@ const Products: React.FC<PageProps> = async ({ params }) => {
     <div className="container mx-auto py-8">
       <h1 className="text-2xl font-bold text-center mb-6">Our Products</h1>
       <div className="flex flex-wrap justify-center">
-        {products.length ? (
-          <Card
-            products={products}
-            onProductSelect={() => {}}
-            isUserLoggedIn={false}
+        {products.length > 0 ? (
+          <Card 
+            products={products} 
+            onProductSelect={() => {}} 
+            isUserLoggedIn={false} 
           />
         ) : (
           <div className="text-gray-500">Products not found</div>
