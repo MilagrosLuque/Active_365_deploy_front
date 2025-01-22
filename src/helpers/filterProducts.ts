a2e09242a97be7935326c62a875a754ea3104619
MilagrosLuque <luquemariamilagros@gmail.com>
2025-01-22 13:14:15 -0300

    verificacion en filterProducts

--- src/helpers/filterProducts.ts
+++ src/helpers/filterProducts.ts
@@ -3,6 +3,6 @@ import { IProducts } from "@/interfaces/IProducts";
 export const filterProducts = (products: IProducts[], searchQuery: string): IProducts[] => {
   if (!searchQuery) return products;
   return products.filter(product => 
-      product.name.toLowerCase().includes(searchQuery.toLowerCase())
+      product.name?.toLowerCase().includes(searchQuery.toLowerCase()) ?? false
   );
 };
